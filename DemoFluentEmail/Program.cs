6fe8a7ccb7245e7435ac85fe8f8b67f32b066a5b
varavchik18 <pashavarava1806@gmail.com>
2024-05-11 19:59:38 +0300

    Implemented basic demo proj to create email file with razor page template on local.

--- DemoFluentEmail/Program.cs
+++ DemoFluentEmail/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+using System.Net.Mail;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+var sender = new SmtpSender(() => new SmtpClient("localhost")
+{
+    EnableSsl = false,
+    DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+    PickupDirectoryLocation = @"D:\Study\Shevchenko\4 course",
+    
+});
+
+StringBuilder template = new();
+template.AppendLine("Dear @Model.FirstName, ");
+template.AppendLine("<p> Thanks for purchasing @Model.ProductName. We hope you enjoy it. </p>");
+template.AppendLine("- The TimCo team");
+
+Email.DefaultSender = sender;
+Email.DefaultRenderer = new RazorRenderer();
+
+var email = await Email
+    .From("tim@timco.com")
+    .To("test@test.com", "Sue")
+    .Subject("Thanks!")
+    .UsingTemplate(template.ToString(), new {FirstName = "Tim", ProductName = "Doritos"})
+    //.Body("Thanks for buying our product")
+    .SendAsync();
